04437914099981e35c65c8859162a14911625173
Reda Tarzalt <tarzaltreda@gmail.com>
2024-09-20 09:34:09 -0400

    update ansible script and setup.yml to continue on errors

--- run_once_install_ansible.sh
+++ run_once_install_ansible.sh
@@ -13,26 +13,32 @@ install_on_ubuntu() {
   sudo apt-get install -y ansible
 }
 
-OS="$(uname -s)"
-case "$OS" in
-  Linux*)
-    if [ -f /etc/arch-release ]; then
-      install_on_arch
-    elif [ -f /etc/lsb-release ] && grep -qi "ubuntu" /etc/lsb-release; then
-      install_on_ubuntu
-    else
-      echo "Unsupported Linux distro"
-      exit 1
-    fi
-      ;;
-    Darwin*)
-      install_on_mac
-      ;;
-    *)
-      echo "Unsupported Linux distro"
-      exit 1
-      ;;
-  esac
+DIRECTORY=$(which ansible)
+
+if [ -z "$DIRECTORY" ]; then
+  OS="$(uname -s)"
+  case "$OS" in
+    Linux*)
+      if [ -f /etc/arch-release ]; then
+          install_on_arch
+      elif [ -f /etc/lsb-release ] && grep -qi "ubuntu" /etc/lsb-release; then
+        install_on_ubuntu
+      else
+        echo "Unsupported Linux distro"
+        exit 1
+      fi
+        ;;
+      Darwin*)
+        install_on_mac
+        ;;
+      *)
+        echo "Unsupported Linux distro"
+        exit 1
+        ;;
+    esac
+else
+  echo "ansible already installed, starting setup"
+fi
 
 ansible-playbook ~/.bootstrap/setup.yml --ask-become-pass
 
