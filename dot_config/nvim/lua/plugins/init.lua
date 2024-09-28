796fa85cc90e65e82cc6ca5f573fa39c13cd58fa
Reda Tarzalt <tarzaltreda@gmail.com>
2024-09-28 12:15:07 -0400

    clean nvim config

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -50,38 +50,7 @@ return {
     end
   },
   {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-      lsp = {
-        signature = {
-          enabled = false,
-        }
-      }
-    },
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      -- OPTIONAL: nvim-notify for notification view
-      "rcarriga/nvim-notify",
-    }
-  }
-
-  -- require("noice").setup({
-  --   lsp = {
-  --     -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-  --     override = {
-  --       ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-  --       ["vim.lsp.util.stylize_markdown"] = true,
-  --       ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-  --     },
-  --   },
-  --   -- you can enable a preset for easier configuration
-  --   presets = {
-  --     bottom_search = true, -- use a classic bottom cmdline for search
-  --     long_message_to_split = true, -- long messages will be sent to a split
-  --     inc_rename = false, -- enables an input dialog for inc-rename.nvim
-  --     lsp_doc_border = false, -- add a border to hover docs and signature help
-  --   },
-  -- })
-
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' }
+  },
 }
