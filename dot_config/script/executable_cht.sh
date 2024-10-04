ab4d5e312b31705e15c59214947c81a3489f458e
Reda Tarzalt <tarzaltreda@gmail.com>
2024-10-04 17:30:30 -0400

    add cht.sh script

---
+++ dot_config/script/executable_cht.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+languages=$(echo "typescript rust bash java python" | tr " " "\n")
+core_utils=$(echo "find xargs sed awk" | tr " " "\n")
+selected=$(echo -e "$languages\n$core_utils" | fzf)
+
+read -p "GIMME QUERY: " query
+
+if echo $languages | grep -qs $selected; then
+  tmux split-window -v bash -c "curl cht.sh/$selected/$(echo "$query" | tr " " "+") | bat --paging=always "
+else
+  tmux split-window -v bash -c "curl cht.sh/$selected~$query/$(echo "$query" | tr " " "+") | less "
+fi
+
