b901683186c3e62c6e920eb02b6165e44d754010
Reda Tarzalt <tarzaltreda@gmail.com>
2024-11-23 14:28:59 -0500

    fix tree-sitter plugin

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,16 +1,38 @@
 return {
   {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
     dependencies = {
-      'windwp/nvim-ts-autotag', -- Autotag dependency
-      'nvim-treesitter/nvim-treesitter-textobjects',
+      "windwp/nvim-ts-autotag", -- Autotag dependency
+      "nvim-treesitter/nvim-treesitter-textobjects",
     },
     config = function()
-      require('nvim-treesitter.configs').setup {
+      require("nvim-treesitter.configs").setup {
         modules = {}, -- Placeholder to avoid warning
         ignore_install = {}, -- Empty table if you don't want to ignore any parsers
-        ensure_installed = { 'c', 'lua', 'vim', 'python', 'html' }, -- Parsers to install
+        ensure_installed = {
+          "json",
+          "javascript",
+          "typescript",
+          "tsx",
+          "yaml",
+          "html",
+          "css",
+          "prisma",
+          "markdown",
+          "markdown_inline",
+          "svelte",
+          "graphql",
+          "bash",
+          "lua",
+          "vim",
+          "dockerfile",
+          "gitignore",
+          "query",
+          "vimdoc",
+          "c",
+        },
         sync_install = false, -- Install languages asynchronously
         auto_install = true, -- Automatically install missing parsers
 
@@ -22,71 +44,71 @@ return {
         incremental_selection = {
           enable = true,
           keymaps = {
-            init_selection = 'ss', -- Start incremental selection
-            node_incremental = 'sn', -- Increment to the next node
-            scope_incremental = 'sc', -- Increment to the next scope (e.g., function or class)
-            node_decremental = 'ds', -- Decrement selection
+            init_selection = "ss", -- Start incremental selection
+            node_incremental = "sn", -- Increment to the next node
+            scope_incremental = "sc", -- Increment to the next scope (e.g., function or class)
+            node_decremental = "ds", -- Decrement selection
           },
         },
         textobjects = {
           swap = {
             enable = true,
             swap_next = {
-              ['<leader>a'] = '@parameter.inner',
+              ["<leader>a"] = "@parameter.inner",
             },
             swap_previous = {
-              ['<leader>A'] = '@parameter.inner',
+              ["<leader>A"] = "@parameter.inner",
             },
           },
           select = {
             enable = true,
             lookahead = true,
             keymaps = {
-              ['ae'] = { query = '@assignment.outer', desc = 'Select assignment with the declaration' },
-              ['ie'] = { query = '@assignment.inner', desc = 'Select assignment without the declaration' },
-              ['le'] = { query = '@assignment.lhs', desc = 'Select left hand side of an assignment' },
-              ['re'] = { query = '@assignment.rhs', desc = 'Select right hand side of an assignment' },
+              ["ae"] = { query = "@assignment.outer", desc = "Select assignment with the declaration" },
+              ["ie"] = { query = "@assignment.inner", desc = "Select assignment without the declaration" },
+              ["le"] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
+              ["re"] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
 
-              ['ap'] = { query = '@parameter.outer', desc = 'Select outer part of a parameter/argument' },
-              ['ip'] = { query = '@parameter.inner', desc = 'Select inner part of a parameter/argument' },
+              ["ap"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/argument" },
+              ["ip"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/argument" },
 
-              ['ai'] = { query = '@conditional.outer', desc = 'Select outer part of a conditional' },
-              ['ii'] = { query = '@conditional.inner', desc = 'Select inner part of a conditional' },
+              ["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
+              ["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
 
-              ['al'] = { query = '@loop.outer', desc = 'Select outer part of a loop' },
-              ['il'] = { query = '@loop.inner', desc = 'Select inner part of a loop' },
+              ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
+              ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
 
-              ['af'] = { query = '@call.outer', desc = 'Select outer part of a function call' },
-              ['if'] = { query = '@call.inner', desc = 'Select inner part of a function call' },
+              ["af"] = { query = "@call.outer", desc = "Select outer part of a function call" },
+              ["if"] = { query = "@call.inner", desc = "Select inner part of a function call" },
 
-              ['am'] = { query = '@function.outer', desc = 'Select outer part of a method/function definition' },
-              ['im'] = { query = '@function.inner', desc = 'Select inner part of a method/function definition' },
+              ["am"] = { query = "@function.outer", desc = "Select outer part of a method/function definition" },
+              ["im"] = { query = "@function.inner", desc = "Select inner part of a method/function definition" },
 
-              ['ac'] = { query = '@class.outer', desc = 'Select outer part of a class' },
-              ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class' },
+              ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
+              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
             },
           },
         },
       }
 
       -- Separate setup for nvim-ts-autotag
-      require('nvim-ts-autotag').setup {
+      require("nvim-ts-autotag").setup {
         filetypes = {
-          'html',
-          'javascript',
-          'typescript',
-          'javascriptreact',
-          'typescriptreact',
-          'svelte',
-          'vue',
-          'tsx',
-          'jsx',
-          'rescript',
-          'css',
-          'lua',
-          'xml',
-          'php',
-          'markdown',
+          "html",
+          "javascript",
+          "typescript",
+          "javascriptreact",
+          "typescriptreact",
+          "svelte",
+          "vue",
+          "tsx",
+          "jsx",
+          "rescript",
+          "css",
+          "lua",
+          "xml",
+          "php",
+          "markdown",
         },
       }
     end,
