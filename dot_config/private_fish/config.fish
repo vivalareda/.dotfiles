202565b074d22cc9f3ea03498c3ec2052bcf7586
Reda Tarzalt <tarzaltreda@gmail.com>
2026-03-21 10:59:55 -0400

    add fish and starship files

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -18,11 +18,13 @@ abbr -a dl yt-dlp
 abbr -a sz 'source ~/.config/fish/config.fish'
 abbr -a ez 'nvim ~/.config/fish/config.fish'
 abbr -a oc opencode
+abbr -a lg lazygit
 
 alias ls 'eza --color=always --icons=always --long --git --no-filesize --no-user --no-permissions'
 alias l 'eza -lh --icons=auto'
 alias ll 'eza -lha --icons=auto --sort=name --group-directories-first'
 alias lt 'eza --icons=auto --tree'
+alias cdgit 'cd ~/github/'
 
 function accept_and_execute
     commandline -f accept-autosuggestion
@@ -36,7 +38,11 @@ bind -M default H beginning-of-line
 bind -M default L end-of-line
 
 set -g fish_key_bindings fish_vi_key_bindings
-set fish_cursor_insert underscore
+
+set fish_cursor_default block
+set fish_cursor_insert block
+set fish_cursor_replace_one block
+set fish_cursor_visual block
 
 if command -v zoxide &>/dev/null
     zoxide init fish | source
