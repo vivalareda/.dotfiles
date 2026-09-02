aaef7df90017bc71e631de1bcb79efb220274ab8
Reda Tarzalt <tarzaltreda@gmail.com>
2026-09-02 09:00:32 +0100

    update config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -23,17 +23,27 @@ abbr -a sz 'source ~/.config/fish/config.fish'
 abbr -a ez 'nvim ~/.config/fish/config.fish'
 abbr -a oc opencode
 abbr -a lg lazygit
+abbr -a rpm repomix
+abbr -a pb pbcopy
+abbr -a o. 'open .'
+abbr -a ju 'jj'
 
 alias ls 'eza --color=always --icons=always --long --git --no-filesize --no-user --no-permissions'
 alias l 'eza -lh --icons=auto'
 alias ll 'eza -lha --icons=auto --sort=name --group-directories-first'
 alias lt 'eza --icons=auto --tree'
 alias cdgit 'cd ~/github/'
+alias cat 'bat --paging=never --style=plain'
+alias mkdir 'mkdir -p'
+alias claude 'claude --dangerously-skip-permissions'
+alias tbnk 'bun /Users/lilflare/github/interpreter-presentation/language/index.ts'
 
 function accept_and_execute
     commandline -f accept-autosuggestion
     commandline -f execute
 end
+
+fzf_configure_bindings --directory=\ef
 
 bind -M insert jk accept-autosuggestion
 bind -M insert jj accept_and_execute
@@ -48,9 +58,12 @@ set fish_cursor_insert block
 set fish_cursor_replace_one block
 set fish_cursor_visual block
 
+starship init fish | source
+
 if command -v zoxide &>/dev/null
     zoxide init fish | source
     alias cd z
 end
 
-starship init fish | source
+# opencode
+fish_add_path /Users/lilflare/.opencode/bin
